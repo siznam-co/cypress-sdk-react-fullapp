dfa2a78379d310aaed56ba2f19054dff6ef43e6c
Osama Aamer <osama.aamer95@gmail.com>
2021-10-07 22:17:05 +0500

    Add react-router

---
+++ src/router/PrivateRoute.tsx
@@ -0,0 +1,27 @@
+import { Route, Redirect } from "react-router-dom";
+
+import { useAuth0 } from "@auth0/auth0-react";
+import Loader from "../components/Loader/Loader";
+
+export default function PrivateRoute({ children, ...rest }: any) {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <Loader />;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
