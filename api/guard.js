6fc52af7e470ec5a4bed5be756a0b3c6ee27454c
Osama Aamer <osama.aamer95@gmail.com>
2021-10-07 23:18:55 +0500

    Add Auth0 guard

---
+++ api/guard.js
@@ -0,0 +1,24 @@
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-2gv2td8l.us.auth0.com/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: "YOUR_API_IDENTIFIER",
+  issuer: [`https://dev-2gv2td8l.us.auth0.com/`],
+  algorithms: ["RS256"],
+});
+
+module.exports = checkJwt;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const checkJwt = require("../guard");
+
+router.get("/integry-keys", checkJwt, function (req, res, next) {
+  console.log(req.user);
+  res.json({
+    appKey: "",
+    appSecret: "",
+    hash: "",
+    userId: "",
+  });
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
