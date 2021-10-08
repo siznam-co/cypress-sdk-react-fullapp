d325bd915b77540b57e0b71b6e194eadba928939
Osama Aamer <osama.aamer95@gmail.com>
2021-10-08 14:02:25 +0500

    Remove SDK related code

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,25 +1,5 @@
 var express = require("express");
-var crypto = require("crypto");
 var router = express.Router();
-
-const checkJwt = require("../guard");
-
-router.get("/integry-keys", checkJwt, async function (req, res, next) {
-  const appKey = process.env.INTEGRY_APP_KEY;
-  const appSecret = process.env.INTEGRY_APP_SECRET;
-  const userId = req.user.sub;
-
-  const hash = crypto
-    .createHmac("sha256", appSecret)
-    .update(userId)
-    .digest("hex");
-
-  res.json({
-    appKey,
-    hash,
-    userId,
-  });
-});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
