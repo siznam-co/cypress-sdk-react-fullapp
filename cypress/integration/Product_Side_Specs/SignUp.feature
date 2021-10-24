40f306c196b7cd0408500034ec46bedf2f334f1c
Muhammad Abdullah <meabdullah13119@gmail.com>
2021-10-24 18:38:48 +0500

    Sign Up All Scenarios
    - Go to the Login screen and verify there is a Sign Up button.
    - Verify Mandatory / Blank fields validations.
    - Verify Invalid fields validations.
    - Verify Already Existing account validation.
    - Sign Up a new user in the Application.

--- cypress/integration/Product_Side_Specs/SignUp.feature
+++ cypress/integration/Product_Side_Specs/SignUp.feature
@@ -2,10 +2,21 @@ Feature: Sign Up Feature
 
     This Feature: contains all the test cases of sign up.
 
-    Scenario: Register a user.
-    Given The user moves to the " ".
-    When the user navigates to the " " screen.
-    Then The user moves to the " ".
-    When The user selects an existing customer.
-        And the user "creates" the " ".
-    Then The user moves to the " ".
+    Scenario: Go to the Login screen and verify there is a Sign Up button.
+    Given The user should be moved to the "signInToDoneDayScreen".
+    When the user hits the "signInToDoneDayBtn" button at "SignIn" screen.
+    Then The user should be moved to the "signInScreen".
+
+    Scenario: Verify Mandatory / Blank fields validations.
+    When the user hits the "signInScreenSignUpBtn" button at "SignIn" screen.
+    Then the user verify "blankFields" at the "SignUp" screen.
+
+    Scenario: Verify Invalid fields validations.
+    Given the user verify "invalidFields" at the "SignUp" screen.
+
+    Scenario: Verify Already Existing account validation.
+    Given the user verify "alreadyExistingAccount" at the "SignUp" screen.
+
+    Scenario: Sign Up a new user in the Application.
+    When the user "createsAccount" at the "SignUp" screen.
+    Then The user should be moved to the "Dashboard".
--- cypress/integration/Product_Side_Specs/SignUp/SignUp.js
+++ cypress/integration/Product_Side_Specs/SignUp/SignUp.js
@@ -1,10 +1,30 @@
 /// <reference types="Cypress" />
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
-import { createOperation, readOperation, submitOperation } from "../../common/helpers"
+import { performOperation, performOperation, performOperation } from "../../common/helpers"
 
-const commonLocators = require("../../../Locators/commonLocators.json")
-const SignUpLocators = require("../../Locators/SignUpLocators.json")
+const commonLocators = require("../../../Locators/commonLocators.json") 
+const SignUpLocators = require("../../../Locators/SignUpLocators.json")
 
 When("the user hits {string} button.", (btn) => {
     cy.get(SignUpLocators[btn]).click() 
+})
+
+When("the user {string} at the {string} screen.", (operation, fieldsType) => {
+    performOperation(operation, fieldsType)
+})
+
+When("the user verify {string} at the {string} screen.", (operation, fieldsType) => {
+    performOperation(operation, fieldsType)
+})
+
+When("the user {string} the {string}.", (operation, fieldsType) => {
+    performOperation(operation, fieldsType)
+})
+
+Then("the {string} appears for the created {string} should be correct.", (operation, fieldsType) => {
+    performOperation(operation, fieldsType)
+})
+
+Then("Initially, the {string} should not be clickable at {string} screen.", (operation, fieldsType) => {
+    performOperation(operation, fieldsType)
 })
