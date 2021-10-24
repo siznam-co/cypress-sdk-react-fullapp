410c1865445556d9f8212cdfa99339b6cfb53e7a
Muhammad Abdullah <meabdullah13119@gmail.com>
2021-10-24 20:10:20 +0500

    Login and Logout hooks in BeforeAll and AfterAll funtions call.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,3 +18,57 @@ Cypress.Commands.add("restoreLocalStorage", () => {
     localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key])
   })
 })
+
+Cypress.Commands.add("loginWithApi", (username, password) => {
+
+    cy.request({
+        method: "POST",
+        url: "/api/auth/login",
+        headers: {
+            "Host": Cypress.config().baseUrl().split("//")[1],
+            "Connection": "keep-alive",
+            "Accept": "application/json, text/plain, */*",
+            // "Authorization": "Bearer undefined",
+            "Origin": "/",
+            "Referer": "/login",
+        },
+        followRedirect: true,
+        form: false,
+        body: { 
+            "email": username, 
+            "password": password
+        }
+    }).then((response) => {
+        expect(response.status).equal(200)
+        // Storing user Data in Cache
+        cy.window().then((window) => {
+            window.localStorage.setItem("profile", JSON.stringify(response.body))
+            cy.log("The user logged in successfully")
+            cy.visit("/login")
+        })
+    })
+})
+
+Cypress.Commands.add("loginWithUI", (username, password) => {
+    cy.visit("/login")
+    // Check if the user is on the login page. 
+    cy.get(SignInLocators.signInToDoneDayBtn).should("be.visible")
+    cy.get(SignInLocators.signInToDoneDayBtn).click()
+
+    // Enter credentials and log in.
+    cy.get(SignInLocators.emailField).type(username)
+    cy.get(SignInLocators.passwordField).type(password)
+
+    cy.get(SignInLocators.submitButton).click()
+})
+
+Cypress.Commands.add("logoutWithUI", () => {
+    cy.get(SignInLocators.logout.profileAvatarBtn).click()
+    cy.get(SignInLocators.logout.signOutOption).click()
+    cy.get(SignInLocators.signInToDoneDayBtn).should("be.visible")
+})
+
+Cypress.Commands.add("runRoutes", () => {
+
+    cy.intercept("GET", "https://app.integry.io/bundle/*").as("getBundle") 
+})
